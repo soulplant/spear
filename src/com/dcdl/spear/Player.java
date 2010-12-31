05f7488b8a4f7d825ae8c696cbdad7299e966edc
James Kozianski <koz@chromium.org>
2010-12-31 12:22:45 +1100

    Player extends Entity.

--- src/com/dcdl/spear/Player.java
+++ src/com/dcdl/spear/Player.java
@@ -3,46 +3,17 @@ package com.dcdl.spear;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
-import com.dcdl.spear.collision.Arena.CollisionCallback;
-import com.dcdl.spear.collision.Arena.CollisionDirection;
-
-public class Player implements CollisionCallback {
+public class Player extends Entity {
   private boolean leftPressed = false;
   private boolean rightPressed = false;
-  private final Entity entity;
 
   public Player(int x, int y, int w, int h) {
-    entity = new Entity(new Rectangle(x * Constants.SCALE, y * Constants.SCALE, w * Constants.SCALE, h * Constants.SCALE));
-  }
-
-  public void moveHorizontal() {
-    entity.moveHorizontal();
-  }
-
-  public void moveVertical() {
-    entity.moveVertical();
-  }
-
-  public CollisionDirection getHorizontalDirection() {
-    return entity.getHorizontalDirection();
-  }
-
-  public CollisionDirection getVerticalDirection() {
-    return entity.getVerticalDirection();
-  }
-
-  public Rectangle getRect() {
-    return entity.getRect();
+    super(new Rectangle(x * Constants.SCALE, y * Constants.SCALE, w * Constants.SCALE, h * Constants.SCALE));
   }
 
   public void render(Graphics g) {
-    Rectangle rect = entity.getRect();
+    Rectangle rect = getRect();
     g.fillRect(rect.x / Constants.SCALE, rect.y / Constants.SCALE, rect.width / Constants.SCALE, rect.height / Constants.SCALE);
-  }
-
-  @Override
-  public void onBounced(CollisionDirection collisionDirection) {
-    entity.onBounced(collisionDirection);
   }
 
   public void left(boolean pressed) {
@@ -56,8 +27,8 @@ public class Player implements CollisionCallback {
   }
 
   public void jump(boolean pressed) {
-    if (entity.isOnFloor() && pressed) {
-      entity.setYVelocity(-2 * Constants.SCALE);
+    if (isOnFloor() && pressed) {
+      setYVelocity(-2 * Constants.SCALE);
     }
   }
 
@@ -69,6 +40,6 @@ public class Player implements CollisionCallback {
     if (rightPressed && !leftPressed) {
       dx = 1 * Constants.SCALE;
     }
-    entity.setXVelocity(dx);
+    setXVelocity(dx);
   }
 }
